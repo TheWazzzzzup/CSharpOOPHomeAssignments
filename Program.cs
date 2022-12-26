c2eed79db6e3e2d9cebc39d8f36c277d5028ee42
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-12-26 22:06:16 +0200

    i didnt something wrong : )

--- Program.cs
+++ Program.cs
@@ -1,17 +1,47 @@
-﻿class Delegates
+﻿
+static void main(string[] args)
 {
-    Predicate<string> LimitedPrerequisite;
 
-    List<string> ObserList;
+    Console.WriteLine("hey");
 
-    void AddToList(Predicate<string> Predicate)
-    {
-        LimitedPrerequisite = Predicate;
-    }
+    LimitedList withoutS = new((s) => !s.StartsWith("s"));
+    withoutS.AddWithPrerequeset("sharon");
+    withoutS.AddWithPrerequeset("notsharon");
+    withoutS.PrintAll();
 
-    void UserAdd(string s)
-    {
-        
-    }
-    
+
 }
+
+public class LimitedList
+{
+    public Action itemAdded;
+
+    List<string> limitedList = new();
+    Func<string, bool> prerequeset;
+
+    public LimitedList(Func<string, bool> listPrerequeset)
+    {
+        prerequeset = listPrerequeset;
+    }
+
+    public bool AddWithPrerequeset(string input)
+    {
+        if (prerequeset.Invoke(input))
+        {
+            limitedList.Add(input);
+            itemAdded?.Invoke();
+            return true;
+        }
+        else return false;
+    }
+
+    public void PrintAll()
+    {
+        foreach (string input in limitedList)
+        {
+            Console.WriteLine(input);
+        }
+    }
+    // this is your code with diffrenet names :)
+}
+
