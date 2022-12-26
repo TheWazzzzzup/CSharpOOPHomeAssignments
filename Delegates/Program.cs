c6002a4e42bc9c2e50db37624774962e4a851f79
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-12-26 22:08:36 +0200

    Dor Copy
    same same different names

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿LimitedList withoutS = new((s) => !s.StartsWith("s"));
+withoutS.AddWithPrerequeset("sharon");
+withoutS.AddWithPrerequeset("shay");
+withoutS.AddWithPrerequeset("shnitzel");
+withoutS.AddWithPrerequeset("rotem");
+
+withoutS.PrintAll();
+public class LimitedList
+{
+    public Action itemAdded;
+
+    List<string> limitedList = new();
+    Func<string, bool> prerequeset;
+
+    public LimitedList(Func<string, bool> listPrerequeset)
+    {
+        prerequeset = listPrerequeset;
+    }
+
+    public bool AddWithPrerequeset(string input)
+    {
+        if (prerequeset.Invoke(input))
+        {
+            limitedList.Add(input);
+            itemAdded?.Invoke();
+            return true;
+        }
+        else return false;
+    }
+
+    public void PrintAll()
+    {
+        foreach (string input in limitedList)
+        {
+            Console.WriteLine(input);
+        }
+    }
+    // this is your code with diffrenet names :)
+}
+
