50516a63eb6bd1f0c3febdbf5ddb2bbf4b68773d
TheWazzzzzup <amitmoryosef@hotmail.com>
2022-12-26 21:27:35 +0200

    Inital

--- Program.cs
+++ Program.cs
@@ -1,57 +1,17 @@
-﻿// - - - - - C# II (Dor Ben Dor) - - - - - - -
-//           Amit Yosef Mor Yosef
-// - - - - - - - - - - - - - - - - - - - - - -
-
-using Brezerkers_Assignemnt;
-
-
-class Game
+﻿class Delegates
 {
-    static public void Main(String[] args)
+    Predicate<string> LimitedPrerequisite;
+
+    List<string> ObserList;
+
+    void AddToList(Predicate<string> Predicate)
     {
-        List<Unit> Ebonies = new();
-        List<Unit> Ivories = new();
-
-        for (int i = 0; i < Random.Shared.Next(1, 20); i++)
-        {
-            int x = Random.Shared.Next(0, 3);
-            {
-                switch (x)
-                {
-                    case 0:
-                        Ebonies.Add(new EbonyBoss());
-                        break;
-                    case 1:
-                        Ebonies.Add(new EbonyHeavy());
-                        break;
-                    case 2:
-                        Ebonies.Add(new EbonyLight());
-                        break;
-
-                }
-            }
-        }
-
-        for (int i = 0; i < Random.Shared.Next(1, 20); i++)
-        {
-            int x = Random.Shared.Next(0, 3);
-            {
-                switch (x)
-                {
-                    case 0:
-                        Ivories.Add(new IvoryHeavy());
-                        break;
-                    case 1:
-                        Ivories.Add(new IvoryLight());
-                        break;
-                    case 2:
-                        Ivories.Add(new TinyNote());
-                        break;
-                }
-            }
-        }
-        Battle.UnitsBattle(Ebonies, Ivories);
+        LimitedPrerequisite = Predicate;
     }
 
-
+    void UserAdd(string s)
+    {
+        
+    }
+    
 }
