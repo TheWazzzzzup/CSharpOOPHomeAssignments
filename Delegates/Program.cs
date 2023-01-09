a87668fc62cdd85ac60adfc4a56774eee2b1cf80
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-01-09 18:11:18 +0200

    2nd WIC attempt

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,45 +1,70 @@
 ﻿/// Generics WIC
 
-interface IGenericList<T>
+class GameObject
 {
-    void Store(T value);
+    List<Component> objectCompenets = new();
 
-    T Retrive(int value);
-
-    bool Unique(T value);
+    T GetComponent<T>() where T: Component
+    {
+        foreach(Component comp in objectCompenets)
+        {
+            if (comp == T)
+        }
+    }
 }
 
-class GenericList<T> : IGenericList<T> where T : class
+
+
+
+
+class Component
 {
-    T[] array = new T[10];
-    int indexer = 0;
 
-    public T Retrive(int index)
-    {
-        return array[index];
-    }
-
-    public void Store(T value)
-    {
-        if (indexer < array.Length)
-        {
-            array[indexer] = value;
-            indexer++;
-        }
-        else return;
-    }
-
-    public bool Unique(T value)
-    {
-        bool isUnique = true;
-        foreach (T item in array)
-        {
-            if (item == value)
-            {
-                isUnique = false;
-                return isUnique;
-            }
-        }
-        return isUnique;
-    }
 }
+
+#region 1st WIC
+//interface IGenericList<T>
+//{
+//    void Store(T value);
+
+//    T Retrive(int value);
+
+//    bool Unique(T value);
+//}
+
+//class GenericList<T> : IGenericList<T> where T : class
+//{
+//    T[] array = new T[10];
+//    int indexer = 0;
+
+//    public T Retrive(int index)
+//    {
+//        return array[index];
+//    }
+
+//    public void Store(T value)
+//    {
+//        if (indexer < array.Length)
+//        {
+//            array[indexer] = value;
+//            indexer++;
+//        }
+//        else return;
+//    }
+
+//    public bool Unique(T value)
+//    {
+//        bool isUnique = true;
+//        foreach (T item in array)
+//        {
+//            if (item == value)
+//            {
+//                isUnique = false;
+//                return isUnique;
+//            }
+//        }
+//        return isUnique;
+//    }
+//}
+
+#endregion
