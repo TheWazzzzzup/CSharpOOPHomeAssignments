89a37423f0e3211425005f4a472186eeac457cb2
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-01-09 17:33:18 +0200

    Interface

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,40 +1,10 @@
-﻿LimitedList withoutS = new((s) => !s.StartsWith("s"));
-withoutS.AddWithPrerequeset("sharon");
-withoutS.AddWithPrerequeset("shay");
-withoutS.AddWithPrerequeset("shnitzel");
-withoutS.AddWithPrerequeset("rotem");
+﻿/// Generics WIC
 
-withoutS.PrintAll();
-public class LimitedList
+interface IGenericList<T>
 {
-    public Action itemAdded;
+    void Store(T value);
 
-    List<string> limitedList = new();
-    Func<string, bool> prerequeset;
+    T Retrive (T value);
 
-    public LimitedList(Func<string, bool> listPrerequeset)
-    {
-        prerequeset = listPrerequeset;
-    }
-
-    public bool AddWithPrerequeset(string input)
-    {
-        if (prerequeset.Invoke(input))
-        {
-            limitedList.Add(input);
-            itemAdded?.Invoke();
-            return true;
-        }
-        else return false;
-    }
-
-    public void PrintAll()
-    {
-        foreach (string input in limitedList)
-        {
-            Console.WriteLine(input);
-        }
-    }
-    // this is your code with diffrenet names :)
+    bool Unique(T value);
 }
-
