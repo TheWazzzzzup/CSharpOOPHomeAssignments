cca2622b81cce47021ce6e7de01b92eca0980965
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-01-09 18:48:01 +0200

    Comments

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿/*
+ * Create a generic Binary Tree
+ * Tree Will Hold 3 fields
+ * 1. generic Self; 2. Generic Left; 3.Generic Right (Left and right may use the same Generic but it is kind of cool if they wont)
+ * Research some more on IEnumerable and IEnumrator 
+ * Create depth-first search, searches Each Branch childs and then moves on
+ * create Breadth-first search, showes each level then moves 
+ */
