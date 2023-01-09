e526238149fc00e644467983116e368f488e5bb3
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-01-09 20:49:12 +0200

    WIC 2
    Extention Method

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using System.Collections;
+﻿using System.Collections;
 
 class Simple<T> : IEnumerable<T>
 {
@@ -31,4 +28,36 @@ struct SimleStruct<T> : IEnumerator<T>
     object IEnumerator.Current => Current;
     public void Dispose() { }
     public void Reset() { }
+
+}
+static class ExtetionMethod
+{
+    static int BoolToInt(this bool b)
+    {
+        return b ? 1 : 0;
+    }
+
+    static int NLettersInString (this string s)
+    {
+        return s.Length;
+    }
+
+    static int FactorialMe(this ref int x)
+    {
+        int f = 0;
+
+        if (x <= 0)
+        {
+            return 0;
+        }
+        else
+        {
+            for (int i = 1; i < x; i++)
+            {
+                f = f * i;
+            }
+        }
+
+        
+    }
 }
