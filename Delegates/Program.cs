a9c243833c93c7404eca77c05b91fe76ea1eee0e
TheWazzzzzup <amitmoryosef@hotmail.com>
2023-01-09 19:10:38 +0200

    Cleanups

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,4 +1,27 @@
-﻿/*
+﻿/// <summary>
+/// Every tree or branch start as a stem, i think 
+/// </summary>
+/// <typeparam name="T1">The self defined value of said branch</typeparam>
+/// <typeparam name="T2">The Type of the Branch sons</typeparam>
+class Stem<T1,T2> where T2 : class
+{
+    T1 Identity { get; set; }
+    T2? LeftChild { get; set; }
+    T2? RightChild { get; set; }
+
+    void DepthFirst()
+    {
+
+    }
+
+    void BreadthFirst()
+    {
+
+    }
+}
+
+
+/*
  * Create a generic Binary Tree
  * Tree Will Hold 3 fields
  * 1. generic Self; 2. Generic Left; 3.Generic Right (Left and right may use the same Generic but it is kind of cool if they wont)
@@ -6,16 +29,3 @@
  * Create depth-first search, searches Each Branch childs and then moves on
  * create Breadth-first search, showes each level then moves 
  */
-
-/// <summary>
-/// 
-/// </summary>
-/// <typeparam name="T1">The self defined value of said branch</typeparam>
-/// <typeparam name="T2">The Type of the Branch</typeparam>
-class Stem<T1,T2>
-{
-    T1 Identity { get; set; }
-
-    T2 LeftChild { get; set; }
-    T2 RightChild { get; set; }
-}
